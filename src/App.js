6f559cc1a4c0e6de2abf3456f274b4caa52cf764
Thinkr1 <146010473+Thinkr1@users.noreply.github.com>
2024-04-06 17:43:45 +0200

    Main logic implemented

--- src/App.js
+++ src/App.js
@@ -4,14 +4,31 @@ import config from "./config.js";
 import "./styles.css";
 
 function Eimg() {
+  const [lon, setLon] = useState("");
+  const [lat, setLat] = useState("");
+  const [date, setDate] = useState("");
+  const [imgData, setImgData] = useState(null)
   
   useEffect(() => {
-    axios
-      .get(`https://api.nasa.gov/planetary/earth/assets?lon=${lon}&lat=${lat}&date=${date}&&dim=0.10&api_key=${config.NASA_API_KEY}`)
-  })
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(`https://api.nasa.gov/planetary/earth/assets?lon=${lon}&lat=${lat}&date=${date}&&dim=0.10&api_key=${config.NASA_API_KEY}`)
+        setImgData(response.data)
+      } catch (err) {
+        console.error("Error fetching data: ", err)
+      }
+    }
+    fetchData()
+  }, [lon, lat,date])
 
   return (
     <div className="container">
+      <form>
+        <input type="number" placeholder="Longitude" value={lon} onChange={(e) => setLon(e.target.value)}/>
+        <input type="number" placeholder="Latitude" value={lat} onChange={(e) => setLat(e.target.value)}/>
+        <input type="text" placeholder="Date (yyyy-mm-dd)" value={date} onChange={(e) => setDate(e.target.value)} pattern="[0-9]{4}-[0-9]{2}-[0-9]{2}"/>
+      </form>
+      {imgData && <img src={imgData.url} alt=""/>}
     </div>
   );
 }
