7dab40e31013c56e0b1dfc1dfa244dab18132ef3
Thinkr1 <146010473+Thinkr1@users.noreply.github.com>
2024-04-06 18:09:54 +0200

    Slider attempt and formatting

--- src/App.js
+++ src/App.js
@@ -6,30 +6,63 @@ import "./styles.css";
 function Eimg() {
   const [lon, setLon] = useState("");
   const [lat, setLat] = useState("");
-  const [dim, setDim] = useState("")
+  const [dim, setDim] = useState("");
   const [date, setDate] = useState("");
-  const [imgData, setImgData] = useState(null)
-  
+  const [imgData, setImgData] = useState(null);
+  const [sliderVal, setSliderVal] = useState(dim);
+
   const handleSubmit = async (e) => {
-    e.preventDefault()
+    e.preventDefault();
     try {
-      const response = await axios.get(`https://api.nasa.gov/planetary/earth/assets?lon=${lon}&lat=${lat}&date=${date}&&dim=${dim}&api_key=${config.NASA_API_KEY}`)
-      setImgData(response.data)
+      const response = await axios.get(
+        `https://api.nasa.gov/planetary/earth/assets?lon=${lon}&lat=${lat}&date=${date}&&dim=${dim}&api_key=${config.NASA_API_KEY}`
+      );
+      setImgData(response.data);
     } catch (err) {
-      console.error("Error fetching data: ", err)
+      console.error("Error fetching data: ", err);
     }
-  }
+  };
 
   return (
     <div className="container">
       <form onSubmit={handleSubmit}>
-        <input type="number" placeholder="Longitude" value={lon} onChange={(e) => setLon(e.target.value)}/>
-        <input type="number" placeholder="Latitude" value={lat} onChange={(e) => setLat(e.target.value)}/>
-        <input type="text" placeholder="Date (yyyy-mm-dd)" value={date} onChange={(e) => setDate(e.target.value)} pattern="[0-9]{4}-[0-9]{2}-[0-9]{2}"/>
-        <input type="range" min="0.025" max="0.2" value={dim} onChange={(e) => setDim(e.target.value)} />
+        <input
+          type="number"
+          placeholder="Longitude"
+          value={lon}
+          onChange={(e) => setLon(e.target.value)}
+        />
+        <input
+          type="number"
+          placeholder="Latitude"
+          value={lat}
+          onChange={(e) => setLat(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Date (yyyy-mm-dd)"
+          value={date}
+          onChange={(e) => setDate(e.target.value)}
+          pattern="[0-9]{4}-[0-9]{2}-[0-9]{2}"
+        />
+        <div className="scontainer">
+          <input
+            className="slider"
+            id="slider"
+            type="range"
+            min="0.025"
+            max="0.2"
+            value={dim}
+            onChange={(e) => {
+              setDim(e.target.value);
+              setSliderVal(e.target.value);
+            }}
+          />
+          <p>{sliderVal}</p>
+        </div>
         <button type="Submit">Submit</button>
       </form>
-      {imgData && <img src={imgData.url} alt=""/>}
+      {imgData && <img src={imgData.url} alt="" />}
     </div>
   );
 }
