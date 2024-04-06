df4e69738b77a81d02e9aa2ac1df3a0eac44b3a4
Thinkr1 <146010473+Thinkr1@users.noreply.github.com>
2024-04-06 17:58:18 +0200

    Added submit and new parameter (dimensions - in degrees))

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import config from "./config.js";
 import "./styles.css";
@@ -6,27 +6,28 @@ import "./styles.css";
 function Eimg() {
   const [lon, setLon] = useState("");
   const [lat, setLat] = useState("");
+  const [dim, setDim] = useState("")
   const [date, setDate] = useState("");
   const [imgData, setImgData] = useState(null)
   
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get(`https://api.nasa.gov/planetary/earth/assets?lon=${lon}&lat=${lat}&date=${date}&&dim=0.10&api_key=${config.NASA_API_KEY}`)
-        setImgData(response.data)
-      } catch (err) {
-        console.error("Error fetching data: ", err)
-      }
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    try {
+      const response = await axios.get(`https://api.nasa.gov/planetary/earth/assets?lon=${lon}&lat=${lat}&date=${date}&&dim=${dim}&api_key=${config.NASA_API_KEY}`)
+      setImgData(response.data)
+    } catch (err) {
+      console.error("Error fetching data: ", err)
     }
-    fetchData()
-  }, [lon, lat,date])
+  }
 
   return (
     <div className="container">
-      <form>
+      <form onSubmit={handleSubmit}>
         <input type="number" placeholder="Longitude" value={lon} onChange={(e) => setLon(e.target.value)}/>
         <input type="number" placeholder="Latitude" value={lat} onChange={(e) => setLat(e.target.value)}/>
         <input type="text" placeholder="Date (yyyy-mm-dd)" value={date} onChange={(e) => setDate(e.target.value)} pattern="[0-9]{4}-[0-9]{2}-[0-9]{2}"/>
+        <input type="range" min="0.025" max="0.2" value={dim} onChange={(e) => setDim(e.target.value)} />
+        <button type="Submit">Submit</button>
       </form>
       {imgData && <img src={imgData.url} alt=""/>}
     </div>
