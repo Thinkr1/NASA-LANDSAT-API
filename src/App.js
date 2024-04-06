8998ddc0f99aa65f5e8e151ed26b77f9adfa152a
Thinkr1 <146010473+Thinkr1@users.noreply.github.com>
2024-04-06 17:19:36 +0200

    Removed template

--- src/App.js
+++ src/App.js
@@ -4,27 +4,14 @@ import config from "./config.js";
 import "./styles.css";
 
 function Eimg() {
+  
   useEffect(() => {
     axios
       .get(`https://api.nasa.gov/planetary/earth/assets?lon=${lon}&lat=${lat}&date=${date}&&dim=0.10&api_key=${config.NASA_API_KEY}`)
   })
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
     </div>
   );
 }
