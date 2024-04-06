557faa93c078320d80f9e3841fca09198f310cb5
Thinkr1 <146010473+Thinkr1@users.noreply.github.com>
2024-04-06 19:06:56 +0200

    Formatting

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import {MapContainer, TileLayer, Marker, Popup} from "react-leaflet"
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import axios from "axios";
 import config from "./config.js";
 import "./styles.css";
@@ -7,23 +7,29 @@ import "./styles.css";
 function Eimg() {
   const [lon, setLon] = useState("");
   const [lat, setLat] = useState("");
-  const [dim, setDim] = useState("")
+  const [dim, setDim] = useState("");
   const [date, setDate] = useState("");
-  const [imgData, setImgData] = useState(null)
-  
+  const [imgData, setImgData] = useState(null);
+
   const handleSubmit = async (e) => {
-    e.preventDefault()
+    e.preventDefault();
     try {
-      const response = await axios.get(`https://api.nasa.gov/planetary/earth/assets?lon=${lon}&lat=${lat}&date=${date}&&dim=${dim}&api_key=${config.NASA_API_KEY}`)
-      setImgData(response.data)
+      const response = await axios.get(
+        `https://api.nasa.gov/planetary/earth/assets?lon=${lon}&lat=${lat}&date=${date}&&dim=${dim}&api_key=${config.NASA_API_KEY}`
+      );
+      setImgData(response.data);
     } catch (err) {
-      console.error("Error fetching data: ", err)
+      console.error("Error fetching data: ", err);
     }
-  }
+  };
 
   return (
     <div className="container">
-      <MapContainer center={[51.505, -0.09]} zoom={13} style={{ height: "400px" }}>
+      <MapContainer
+        center={[51.505, -0.09]}
+        zoom={13}
+        style={{ height: "400px" }}
+      >
         <TileLayer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -36,14 +42,37 @@ function Eimg() {
         </Marker> */}
       </MapContainer>
       <form onSubmit={handleSubmit}>
-        <input type="number" placeholder="Longitude" value={lon} onChange={(e) => setLon(e.target.value)}/>
-        <input type="number" placeholder="Latitude" value={lat} onChange={(e) => setLat(e.target.value)}/>
-        <input type="text" placeholder="Date (yyyy-mm-dd)" value={date} onChange={(e) => setDate(e.target.value)} pattern="[0-9]{4}-[0-9]{2}-[0-9]{2}"/>
+        <input
+          type="number"
+          placeholder="Longitude"
+          value={lon}
+          onChange={(e) => setLon(e.target.value)}
+        />
+        <input
+          type="number"
+          placeholder="Latitude"
+          value={lat}
+          onChange={(e) => setLat(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Date (yyyy-mm-dd)"
+          value={date}
+          onChange={(e) => setDate(e.target.value)}
+          pattern="[0-9]{4}-[0-9]{2}-[0-9]{2}"
+        />
         {/* <input type="range" min="0.025" max="0.2" value={dim} onChange={(e) => setDim(e.target.value)} /> */}
-        <input type="number" placeholder="Dimensions" value={dim} onChange={(e) => setDim(e.target.value)} />
-        <button className="submit-btn" type="Submit">Submit</button>
+        <input
+          type="number"
+          placeholder="Dimensions"
+          value={dim}
+          onChange={(e) => setDim(e.target.value)}
+        />
+        <button className="submit-btn" type="Submit">
+          Submit
+        </button>
       </form>
-      {imgData && <img src={imgData.url} alt="" className="img"/>}
+      {imgData && <img src={imgData.url} alt="" className="img" />}
     </div>
   );
 }
